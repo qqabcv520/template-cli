2ce39eeae20ac9012a3ad1fc79c803aa4e20bf90
qqabcv520 <605655316@qq.com>
2019-04-12 18:26:29 +0800

    忽略dist

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import 'core-js/es7/array';
 import { generate } from './generate';
 
 program
-    .version(require('../package.json').version, '-v, --version')  // tslint:disable-line
-    .usage('<command> [options]');
+  .version(require('../package.json').version, '-v, --version')  // tslint:disable-line
+  .usage('<command> [options]');
 
 program
-    .command('generate')
-    .alias('g')
-    .description('generate a new project or module or component.')
-    .action(generate);
+  .command('generate')
+  .alias('g')
+  .description('生成一个新模块或组件')
+  .action(generate);
 
 program.parse(process.argv);
