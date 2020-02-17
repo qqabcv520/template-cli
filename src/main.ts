bcab54f87f32737570cb75916a7ca4e2c4bdbc8c
qqabcv520 <605655316@qq.com>
2020-02-17 18:00:18 +0800

    chore: 生成组件展示description
    --task=1440795 --user=范子才 【前端】topsale列表页... https://www.tapd.cn/55786408/s/3541195

--- src/main.ts
+++ src/main.ts
@@ -13,19 +13,19 @@ program
 program
   .command('generate')
   .alias('g')
-  .description('生成一个新模块或组件')
+  .description('生成一个新模块或组件1')
   .action(generate);
 
 program
   .command('project')
   .alias('p')
-  .description('生成一个新项目')
+  .description('生成一个新项目2')
   .action(generateProject);
 
 program
   .command('api')
   .alias('a')
-  .description('生成项目接口和数据实体')
+  .description('生成项目接口和数据实体3')
   .action(generateAPI);
 
 program.parse(process.argv);
